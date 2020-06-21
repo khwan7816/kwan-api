a8db9bbb5eb82689b1d98099edff4f0e51aedb96
khwan <rudghks7816@gmail.com>
2020-06-21 22:51:50 +0900

    - test: env 설정 테스트

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import UserRouter from './Users';
+import dotenv from 'dotenv';
 
 // Init router and path
 const router = Router();
+
+dotenv.config();
+console.log(process.env.NODE_ENV)
+console.log(process.env.PORT)
+console.log(process.env.HOST)
 
 // Add sub-routes
 router.use('/users', UserRouter);
